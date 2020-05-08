63f969f21f988cf9aee7645d4c9252236758064c
romain <romain.pillot@epitech.eu>
2020-05-08 19:58:36 +0100

    adds alpharite entry point

--- alpharite-sdk/src/Main.cs
+++ alpharite-sdk/src/Main.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UnityEngine;
+
 namespace AlphaRite
 {
-    public class Main
+    public class Main: MonoBehaviour {
+        private static GameObject alphaRiteInstance;
 
-    {
+        public void Start() {
 
+        }
+
+        public void Update() {
+
+        }
+
+        public void OnGUI() {
+
+        }
+
+        public static void AlphaRiteEntryPoint() {
+            Main.alphaRiteInstance = new GameObject();
+            Main.alphaRiteInstance.AddComponent<Main>();
+
+            GameObject.DontDestroyOnLoad(Main.alphaRiteInstance);
+        }
+
+        public static void AlphaRiteDestroy() {
+            GameObject.Destroy(Main.alphaRiteInstance);
+        }
     }
 }
