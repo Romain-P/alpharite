e2803ece9092c7284aa3c99d12e0a03a2222a010
romain <romain.pillot@epitech.eu>
2020-05-09 22:58:23 +0100

    stable version, script injected and called ingame

--- alpharite-sdk/AlphaRite/src/sdk/hacks/WallHack.cs
+++ alpharite-sdk/AlphaRite/src/sdk/hacks/WallHack.cs
@@ -4,29 +4,23 @@ using Gameplay.GameObjects;
 using MergedUnity.Glues;
 using MergedUnity.Glues.GUI;
 using StunShared.GlueSystem;
+ using UnityEngine;
 
-namespace AlphaRite.sdk.hacks {
+ namespace AlphaRite.sdk.hacks {
     public class WallHack: AlphaCycle {
         public WallHack(AlphariteSdk sdk) : base(sdk) {}
 
         protected override void onStart() {
-            DataTableSystem dataTableSystem = sdk.references.datatableSystem.Instance;
-            var data = new DataTable(dataTableSystem, dataTableSystem.Create(GameObjectTypeId.Empty));
-            
-            data.Set("Enabled", false);
-            sdk.references.gameClient.Instance.Debug("fogofwar", data, false);
+            Debug.LogError("created");
         }
 
         protected override void onStop() {
-            DataTableSystem dataTableSystem = sdk.references.datatableSystem.Instance;
-            var data = new DataTable(dataTableSystem, dataTableSystem.Create(GameObjectTypeId.Empty));
-            
-            data.Set("Enabled", true);
-            sdk.references.gameClient.Instance.Debug("fogofwar", data, false);
+            Debug.LogError("stopped");
         }
 
         protected override void onUpdate() {
-            sdk.references.gameClient.Instance.SendChatMessage("fuck you", true);
+            Debug.LogError("test");
+            //sdk.references.gameClient.Instance.SendChatMessage("fuck you", true);
         }
     }
 }
