b53a1962fe9e960ce3ca7c9b62a2e5d94dfcc4b6
romain <romain.pillot@epitech.eu>
2020-05-09 06:06:35 +0100

    first release, ends loader & alpharite script class

---
+++ alpharite-sdk/src/Loader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AlphaRite
+{
+    public class AlphaRiteLoader: MonoBehaviour {
+        private static GameObject _alphaRiteInstance;
+        
+        public static void init() {
+            _alphaRiteInstance = new GameObject();
+            _alphaRiteInstance.AddComponent<ScriptAdapter>();
+
+            GameObject.DontDestroyOnLoad(_alphaRiteInstance);
+        }
+
+        public static void stop() {
+            GameObject.Destroy(_alphaRiteInstance);
+        }
+    }
+}
---
+++ alpharite-sdk/src/ScriptAdapter.cs
@@ -0,0 +1,25 @@
+﻿using AlphaRite.sdk;
+using UnityEngine;
+
+namespace AlphaRite {
+    public class ScriptAdapter: MonoBehaviour{
+        private AlphariteSdk _sdk;
+        
+        public void Start() {
+            _sdk = new AlphariteSdk();
+            _sdk.onStart();
+        }
+
+        public void Update() {
+            _sdk.onUpdate();
+        }
+
+        public void OnGUI() {
+            _sdk.onRenderingUpdate();
+        }
+
+        public void OnDisable() {
+            _sdk.onStop();
+        }
+    }
+}
